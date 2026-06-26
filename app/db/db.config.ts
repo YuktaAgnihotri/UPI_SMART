5b5dab3d85194a053096e9de8217fdeedb5691c4
YuktaAgnihotri <yuktaagnihotri8585@gmail.com>
2026-06-26 14:53:08 +0530

    chaging app.config.ts

--- app/db/db.config.ts
+++ app/db/db.config.ts
@@ -1,15 +1,28 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+ import { PrismaPg } from "@prisma/adapter-pg";
+import  {PrismaClient} from "@prisma/client/extension";
 
 
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
-})
+ const prisma = new PrismaClient({
+   adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
+ })
 
-export default prisma;
-// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+ export default prisma;
+
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
 
 // export const prisma =
-//   globalForPrisma.prisma ??  new PrismaClient();
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+//   });
 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
+
+// export default prisma;
